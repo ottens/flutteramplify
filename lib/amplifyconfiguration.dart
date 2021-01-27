0d5ce91545b2890c753f022274c39e736375a287
Ottens, Maarten <maarten_ottens@fanniemae.com>
2021-01-27 10:38:18 -0500

    amplifying

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -4,9 +4,8 @@ const amplifyconfig = ''' {
     "api": {
         "plugins": {
             "awsAPIPlugin": {
-                "myamplifyproject": {
+                "flutteramplify": {
                     "endpointType": "GraphQL",
-                    "endpoint": "https://7slzs4ldvvgl7caglfpft735di.appsync-api.us-east-1.amazonaws.com/graphql",
                     "region": "us-east-1",
                     "authorizationType": "AMAZON_COGNITO_USER_POOLS"
                 }
@@ -20,45 +19,7 @@ const amplifyconfig = ''' {
                 "Version": "0.1.0",
                 "IdentityManager": {
                     "Default": {}
-                },
-                "CredentialsProvider": {
-                    "CognitoIdentity": {
-                        "Default": {
-                            "PoolId": "us-east-1:dbb3af2a-c28b-426e-97a7-e670060e9eb3",
-                            "Region": "us-east-1"
-                        }
-                    }
-                },
-                "CognitoUserPool": {
-                    "Default": {
-                        "PoolId": "us-east-1_Kzf2HQjMc",
-                        "AppClientId": "28csktqaqa46t5o6h4lq1lsdpb",
-                        "AppClientSecret": "1ij9ipqjqioqs619c88v7u0mnuq2gb5h4lr28hu8hjea40ev2ckk",
-                        "Region": "us-east-1"
-                    }
-                },
-                "Auth": {
-                    "Default": {
-                        "authenticationFlowType": "USER_SRP_AUTH"
-                    }
-                },
-                "AppSync": {
-                    "Default": {
-                        "ApiUrl": "https://7slzs4ldvvgl7caglfpft735di.appsync-api.us-east-1.amazonaws.com/graphql",
-                        "Region": "us-east-1",
-                        "AuthMode": "AMAZON_COGNITO_USER_POOLS",
-                        "ClientDatabasePrefix": "myamplifyproject_AMAZON_COGNITO_USER_POOLS"
-                    }
                 }
-            }
-        }
-    },
-    "storage": {
-        "plugins": {
-            "awsS3StoragePlugin": {
-                "bucket": "myamplifyproject185e7f83468a4b20a7fc6a5f92a3406111003-dev",
-                "region": "us-east-1",
-                "defaultAccessLevel": "guest"
             }
         }
     }
